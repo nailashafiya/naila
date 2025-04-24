ccbeb6305a28d1b6e623d31f5d547b4d578f657c
nailashafiya <nai27.sf@gmail.com>
2025-04-24 11:01:45 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,4 @@
-import streamlit as st
+import streamlit "
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.title("🐕 Tralala")
+st.write("Latihan membuat website melalui Githuh dan Streamlit dengan menggunakan bahasa Pyhton")
