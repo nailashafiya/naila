c71c79e48a4b804a91cc95ef608c14d765c85883
nailashafiya <nai27.sf@gmail.com>
2025-05-22 21:17:55 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,4 @@ import streamlit "
 
 st.title("🐕 Tralala")
 st.write("Latihan membuat website melalui Githuh dan Streamlit dengan menggunakan bahasa Pyhton")
+st.image("1d587fe77b8554f82cfef273006106a8 (1).jpg", widht=200)
